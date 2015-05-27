dda5ef7c6fa0f8df5d9172fbbceca942cc38d64e
Lonnie Ezell <lonnieje@gmail.com>
2015-05-26 22:59:04 -0500

    Getting PHPUnit setup and ready to go.

---
+++ tests/BayTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lonnie
+ * Date: 5/26/15
+ * Time: 10:52 PM
+ */
+
+namespace Myth\Bay;
+
+
+class BayTest extends \PHPUnit_Framework_TestCase {
+
+
+}
